9c5ea57d3cfc5bc357991c8e9bbea3f1d17a6d21
Rayane <sirri.rayane.ma@gmail.com>
2026-04-21 20:49:48 +0100

    User system UI : Themes toggle, settings part 2

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,10 @@
 document.addEventListener("click", (ev) => {
+
+    //Settings container
+    if (ev.target.classList.contains("settingsContainer")) {
+        document.getElementById("settingsContainer").classList.remove("show")
+    }
+
     if (ev.target.classList.contains("settingsBTN")) {
         document.getElementById("settingsContainer").classList.add("show")
     }
@@ -7,3 +13,33 @@ document.addEventListener("click", (ev) => {
         document.getElementById("settingsContainer").classList.remove("show")
     }
 })
+
+let theme = localStorage.getItem("theme") || "dark"
+if (theme) { 
+    document.querySelector(".window").dataset.theme = theme 
+    document.querySelector("#settingsContainer").dataset.theme = theme
+}
+
+function toggleTheme() {
+    if (theme == "dark") {
+        theme = "light"
+        localStorage.setItem("theme", "light")
+        document.querySelector(".window").dataset.theme = theme
+        document.querySelector("#settingsContainer").dataset.theme = theme
+    } else {
+        theme = "dark"
+        localStorage.setItem("theme", "dark")
+        document.querySelector(".window").dataset.theme = theme
+        document.querySelector("#settingsContainer").dataset.theme = theme
+    }
+}
+
+/* Database side will be added later
+async function getTheme() {
+
+}
+
+async function setTheme() {
+
+}
+*/
