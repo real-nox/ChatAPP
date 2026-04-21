ff27c7b75e40d91ccd7e2dbe15e64987296a6a8d
Rayane <sirri.rayane.ma@gmail.com>
2026-04-21 19:18:56 +0100

    User system UI : Final version of register side. Password checking, if it includes numbers, uppercase letters, lowercase letters and special characters.

--- public/register.js
+++ public/register.js
@@ -4,6 +4,12 @@ const valid = {
     pwdcheck: false
 }
 
+const username_special_char_reg = /^[a-z1-Z0-9._-]+$/
+const special_char_reg = /[!@#$%^&*()\-_=+\[\]{};':",.<>/?\\|]/
+const num_range_reg = /[0-9]/
+const cap_letters_char = /[A-Z]/
+const low_letters_char = /[a-z]/
+
 const passwordCheck = document.getElementById("passwordCheck")
 const password = document.getElementById("pwd")
 const username = document.getElementById("username")
@@ -34,9 +40,10 @@ function CheckValid() {
 }
 
 username.addEventListener("input", (ev) => {
-    console.log(ev.target.value)
-    if (ev.target.value != ev.target.value.toLowerCase() || ev.target.value.includes(" ") || ev.target.value.match(/[!@#$%^&*(),?":{}|<>]/g)) {
-        username_error.innerText = "Username should contain lowercase letters or '_' '.'"
+    const chars = ev.target.value
+    console.log(chars)
+    if (!username_special_char_reg.test(chars)) {
+        username_error.innerText = "Only letters, numbers, . _ - allowed"
         valid.username = false
     } else {
         username_error.innerText = ""
@@ -47,16 +54,82 @@ username.addEventListener("input", (ev) => {
 })
 
 password.addEventListener("input", (ev) => {
-    console.log(ev.target.value.length)
-    if (ev.target.value.length < 5) {
-        document.getElementById("CheckingPWD").innerText = "Weak password."
-        valid.password = false
-    } else {
-        document.getElementById("CheckingPWD").innerText = ""
-        valid.password = true
+    const chars = ev.target.value
+    const points = {
+        capital_l: false,
+        lower_l: false,
+        special_char_included: false,
+        numbers: false
     }
 
-    CheckValid()
+    if (cap_letters_char.test(chars)) {
+        points.capital_l = true
+        document.querySelector(".requirement.up").classList.add("done")
+    } else {
+        points.capital_l = false
+        document.querySelector(".requirement.up").classList.remove("done")
+    }
+
+    if (low_letters_char.test(chars)) {
+        points.lower_l = true
+        document.querySelector(".requirement.low").classList.add("done")
+    }
+    else {
+        points.lower_l = false
+        document.querySelector(".requirement.low").classList.remove("done")
+    }
+
+    if (special_char_reg.test(chars)) {
+        points.special_char_included = true
+        document.querySelector(".requirement.sp").classList.add("done")
+    } else {
+        points.special_char_included = false
+        document.querySelector(".requirement.sp").classList.remove("done")
+    }
+
+    if (num_range_reg.test(chars)) {
+        points.numbers = true
+        document.querySelector(".requirement.n").classList.add("done")
+    } else {
+        points.numbers = false
+        document.querySelector(".requirement.n").classList.remove("done")
+    }
+
+    const score = Object.values(points).filter(Boolean).length
+
+    const pwdC = document.getElementById("CheckingPWD")
+    if (chars.length < 5) {
+        pwdC.innerText = "Weak password"
+        pwdC.style.color = "rgb(161, 161, 161)"
+        valid.password = false
+    } else {
+        switch (score) {
+            case 1:
+                pwdC.innerText = "Weak password"
+                pwdC.style.color = "rgb(117, 117, 117)"
+                valid.password = false
+                break;
+
+            case 2:
+                pwdC.innerText = "Medium password"
+                pwdC.style.color = "rgb(38, 135, 112)"
+                valid.password = false
+                break;
+
+            case 3:
+                pwdC.innerText = "Good password"
+                pwdC.style.color = "rgb(23, 109, 170)"
+                valid.password = false
+                break;
+
+            case 4:
+                pwdC.innerText = "Strong password"
+                pwdC.style.color = "rgb(14, 7, 227)"
+                valid.password = true
+                break;
+        }
+        CheckValid()
+    }
 })
 
 passwordCheck.addEventListener("input", (ev) => {
